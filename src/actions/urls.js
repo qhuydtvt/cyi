6f6b24c66e673d71f409db1c42dbba7e139238dd
huynq <qhuydtvt@gmail.com>
2018-05-20 09:26:22 +0700

    .

--- src/actions/urls.js
+++ src/actions/urls.js
@@ -1,4 +1,4 @@
-export const ROOT_URL = "http://54.179.166.244:5000"
+export const ROOT_URL = "http://54.179.183.162:5000"
 export const API_URL = `${ROOT_URL}/api`
 export const MANAGEMENT_API_URL = `${API_URL}/management`;
 export const DEPARTMENT_API_URL = `${MANAGEMENT_API_URL}/department`;
